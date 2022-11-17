6cac04c24dd6f646385e4474bdb394039054210a
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2022-11-18 00:42:56 +0400

    opt92348321523

--- js/script.js
+++ js/script.js
@@ -16,3 +16,37 @@ openMenu.addEventListener("click", function () {
 closeMenu.addEventListener("click", function () {
   navLinks.classList.remove("hidden");
 });
+
+const navbar = document.querySelector(".header--navbar-nav");
+
+navbar.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("header--navbar-link")) {
+    const id = e.target.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// const header = document.querySelector(".header");
+
+// const nav = document.querySelector(".header--navbar");
+// const navHeight = nav.getBoundingClientRect().height;
+
+// const navSticky = function (entries) {
+//   const [entry] = entries;
+
+//   if (!entry.isIntersecting) {
+//     nav.classList.add("sticky");
+//   } else {
+//     nav.classList.remove("sticky");
+//   }
+// };
+// const navObserver = new IntersectionObserver(navSticky, {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: `-${navHeight}px`,
+// });
+
+// navObserver.observe(header);
