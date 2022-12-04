40fa6c3aa91821fd3a9333780c8d597d7aed0c43
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2022-12-04 23:10:16 +0400

    opt09589205235

--- js/script.js
+++ js/script.js
@@ -68,7 +68,7 @@ const revealSection = function (entries, observer) {
 };
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.2,
+  threshold: 0.15,
 });
 
 allSections.forEach((section) => {
